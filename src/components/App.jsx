abdb3cc2c2ba7960e76d69eb2bccb46b6cab0f95
alexmndzdev <alejandro.mendez.developer@gmail.com>
2020-11-06 18:49:34 -0600

    ADD | Hello world example with Alan

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import Footer from './Footer/Footer';
 import { PortfolioProvider } from '../context/context';
 
 import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
 function App() {
   const [hero, setHero] = useState({});
@@ -15,6 +16,7 @@ function App() {
   const [projects, setProjects] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
+  const alanKey = '8ea6e8b5dedfb4de5e2cf8e770d32a5b2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
   useEffect(() => {
     setHero({ ...heroData });
@@ -22,6 +24,14 @@ function App() {
     setProjects([...projectsData]);
     setContact({ ...contactData });
     setFooter({ ...footerData });
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === 'Download') {
+          alert('Descargando...')
+        }
+      }
+    })
   }, []);
 
   return (
