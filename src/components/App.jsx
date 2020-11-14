3c2be2a5aa44eaa63f310468eaf240dd1c44c2b7
alexmndzdev <alejandro.mendez.developer@gmail.com>
2020-11-14 17:56:47 -0600

    MOD | added cv url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,6 +29,7 @@ function App() {
       onCommand: ({ command }) => {
         if (command === 'Download') {
           alert('Descargando...')
+          window.open('https://drive.google.com/uc?download=export&id=1_3sx3BjfnqDJOzTllliknOhb8w1uHXh5', '_blank')
         }
       }
     })
