e28a770c60dd60626571f4afca33dfcde7f837b5
alexmndzdev <alejandro.mendez.developer@gmail.com>
2020-11-16 19:30:40 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4fedda971210818a0dc2.js"
+    "url": "webpack-runtime-665bf0fad033504466e8.js"
   },
   {
     "url": "framework-dac95de3c301f57a114a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "17fe31ffb1cfb0ca37ba222febf9738f"
+    "revision": "88bc0a4a51d06a6bbd276000f52df08d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "111e516c0a72cb8912e8fc1e98bb3df3"
+    "revision": "ccdcff6da5b43e65ed0b965172954c52"
   },
   {
     "url": "polyfill-4b1e8b7e04c413f9811f.js"
